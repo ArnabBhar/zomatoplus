678dca8d674fee6af278b65ffdeb5462a7afb327
Arnab <Arnab@DESKTOP-BUI9GP7>
2018-06-27 22:02:06 +0530

    Commit

--- src/main/java/com/thinkxfactor/zomatoplus/controller/UserController.java
+++ src/main/java/com/thinkxfactor/zomatoplus/controller/UserController.java
@@ -1,22 +1,49 @@
 package com.thinkxfactor.zomatoplus.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thinkxfactor.zomatoplus.models.User;
+import com.thinkxfactor.zomatoplus.models.UserNotFoundException;
+import com.thinkxfactor.zomatoplus.repository.UserRepository;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
 	
-
-	@GetMapping("/login")
-	public String userLogin() {
-		return "Hello from thinkxfactor";
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("/add")
+	public User addUser(@RequestBody User user) {
+		User persistedUser=userRepository.save(user);
+		return persistedUser;
 	}
 
 	
 
+	@PostMapping("/login")
+	public User userLogin(String name, String password) throws UserNotFoundException{
+		User user = userRepository.findByNameAndPassword(name,password);
+		 if (user == null) {
+	            throw new UserNotFoundException();
+	        }
+		return user;
+		}
+	
+	@GetMapping("/getAll")
+	public List<User> getAll(){
+		List<User> listofusers=userRepository.findAll();
+		
+		return listofusers;
+	}
+	
 }
